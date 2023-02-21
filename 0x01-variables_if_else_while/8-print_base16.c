36ed6f268c6e90836f6105198273dd15d74f683e
MAOpsalms <MichaelAnanOfficial@gmail.com>
2023-02-20 16:47:40 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i = 0;
+
+	while (i < 16)
+	{
+		if (i < 10)
+			putchar(i + '0');
+		else
+			putchar(i + 'W');
+		i++;
+	}
+
+	putchar('\n');
+	return (0);
+}
+
